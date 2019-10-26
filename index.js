4f443d403c6fb02a520bd6134094f1f3b36a0120
Alejandro Lopez <alejozepol@gmail.com>
2019-10-25 19:40:26 -0500

    capa de manejo de errores por middleware

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const app = express();
+
 const { config } = require('./config/index');
-const moviesApi = require('./router/movies');
+const moviesApi = require('./routes/movies');
 
+const { logErrors, errorHandler } = require('./utils/middlewere/errorHandlers');
 
+//body parser
 app.use(express.json());
 
 moviesApi(app);
-app.listen(config.port, function() {
+
+app.use(logErrors);
+app.use(errorHandler)
+
+app.listen(config.port, function () {
   // eslint-disable-next-line no-console
   console.log(`listening http://localhost:${config.port}`);
 });
