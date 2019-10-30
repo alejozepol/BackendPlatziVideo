cf0c4eed5ccf41e58ae99a47c1d5ff6f4ccb5ebb
Alejandro Lopez <alejozepol@gmail.com>
2019-10-29 19:14:47 -0500

    test get movies

--- utils/mocks/movile.js
+++ utils/mocks/movile.js
@@ -702,6 +702,24 @@ const moviesMock =[
   }
 ];
 
+function filteredMoviesMocks(tag){
+  return moviesMock.filter((movie) => movie.tag.includes(tag))
+}
+
+class MoviesServiceMock{
+
+  async getMovies(){
+    return Promise.resolve(moviesMock)
+  }
+
+  async createMovie(){
+    return Promise.resolve(moviesMock[0])
+  }
+}
+
+
 module.exports = {
-  moviesMock
+  moviesMock,
+  filteredMoviesMocks,
+  MoviesServiceMock
 }
