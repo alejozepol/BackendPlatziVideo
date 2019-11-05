d3f06acf0af2ca64f6bad58422d8a1f8e9f971bd
Alejandro Lopez <alejozepol@gmail.com>
2019-11-05 16:27:01 -0500

    creracion test de lista para los movies

--- test/routes.movies.test.js
+++ test/routes.movies.test.js
@@ -1,7 +1,7 @@
-/* const assert = require('assert'); */
+const assert = require('assert');
 const proxyquire = require('proxyquire');
 
-const { MoviesServiceMock } = require('../utils/mocks/movies');
+const { MoviesServiceMock, moviesMock } = require('../utils/mocks/movies');
 const testServer = require('../utils/testServer');
 
 describe('routes - movies', function () {
@@ -14,5 +14,16 @@ describe('routes - movies', function () {
     it('should respond with status 200', function (done) {
       request.get('/api/movies').expect(200, done)
     })
+
+    it('should respond with the list of movies', function (done){
+      request.get('/api/movies').end((err, res) =>{
+        assert.deepEqual(res.body, {
+          data: moviesMock,
+          message: 'movies listed' 
+        })
+      })
+      done()
+    })
+
   })
 })
